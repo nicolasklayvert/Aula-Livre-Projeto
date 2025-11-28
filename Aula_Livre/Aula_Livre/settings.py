63b7a14fa48f62e5d015ca53fd8802862b606b21
DiegoL13 <diegolps1317@gmail.com>
2025-11-28 12:38:59 -0300

    Integracao completa 100% gostosinha

--- Aula_Livre/Aula_Livre/settings.py
+++ Aula_Livre/Aula_Livre/settings.py
@@ -52,9 +52,9 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'Aula_Livre.urls'
 
 TEMPLATES = [
-    {
+    {   
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'Aula_Livre' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,6 +115,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
